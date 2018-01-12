4e7a7d95c289a9e1c9cc2ca22f05a09c31ac6e22
Matthew Victor <vicmcnabby@gmail.com>
2018-01-11 21:58:04 -0700

    adjusted some routes

--- api/router.js
+++ api/router.js
@@ -68,4 +68,52 @@ router.get('/users', (req, res, next) => {
   });
 });
 
+router.get('/users/:id/lists', (req, res) => {
+  queries.getListsByUserId(req.params.id).then(lists => {
+    res.json(lists);
+  });
+});
+
+router.get('/lists/:id/items', (req, res) => {
+  queries.getItemsByListId(req.params.id).then(items => {
+    res.json(items);
+  });
+});
+
+router.post('/list', (req, res, next) => {
+  queries.createItem('list', req.body).then(response => {
+    res.json(response);
+  });
+});
+
+router.post('/item', (req, res, next) => {
+  queries.createItem('item', req.body).then(response => {
+    res.json(response);
+  });
+});
+
+router.put('/list/:id', (req, res, next) => {
+  queries.updateItem('list', req.params.id, req.body).then(response => {
+    res.json(response);
+  });
+});
+
+router.put('/item/:id', (req, res, next) => {
+  queries.updateItem('item', req.params.id, req.body).then(response => {
+    res.json(response);
+  });
+});
+
+router.delete('/list/:id', (req, res, next) => {
+  queries.deleteItem('list', req.params.id).then(response => {
+    res.json(response);
+  });
+});
+
+router.delete('/item/:id', (req, res, next) => {
+  queries.deleteItem('item', req.params.id).then(response => {
+    res.json(response);
+  });
+});
+
 module.exports = router;
--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,30 @@
 const knex = require('./knex');
 
 module.exports = {
+  createUser(user) {
+    return knex('users').insert(user, '*');
+  },
+  getUserByEmail(email) {
+    return knex('users').where('email', email).first();
+  },
   getAllUsers() {
     return knex('users');
+  },
+  getListsByUserId(id) {
+    return knex('list').where('users_id', id)
+      .join('users_list', 'list_id', '=', 'list.id');
+  },
+  getItemsByListId(id) {
+    return knex('item').where('list_id', id)
+    // .join('list', 'list.id', '=', 'list_id');
+  },
+  createItem(tableName, item) {
+    return knex(tableName).insert(item, '*');
+  },
+  deleteItem(tableName, id) {
+    return knex(tableName).where('id', id).del();
+  },
+  updateItem(tableName, id, item) {
+    return knex(tableName).where('id', id).update(item, '*');
   }
 }
